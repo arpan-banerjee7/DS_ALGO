e4864a1733006c7aba17f08d77d8741447bcdf4e
Arpan <arpan.nbanerjee7@gmail.com>
2021-09-01 10:38:53 +0530

    added clear solution for split array prob

--- Arrays/src/array/questions/binarysearch/SplitArraySum.java
+++ Arrays/src/array/questions/binarysearch/SplitArraySum.java
@@ -1,7 +1,10 @@
 package array.questions.binarysearch;
 
 // https://leetcode.com/problems/split-array-largest-sum/
-// TC-- o(n)_o(logn)~o(n)
+// https://www.geeksforgeeks.org/painters-partition-problem-set-2/
+// https://www.geeksforgeeks.org/allocate-minimum-number-pages/
+
+// TC--  O(N * log (sum (arr[]))  
 public class SplitArraySum {
 	public static int splitArray(int[] nums, int m) {
 		int start = 0;
@@ -39,6 +42,91 @@ public class SplitArraySum {
 
 		}
 		return end; // here start == end
+	}
+
+	public int splitArray2(int[] nums, int m) {
+		int start = 0;
+		int end = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item fro the array
+			end += nums[i];
+		}
+		int ans = -1;
+		// binary search
+		while (start <= end) {
+			// try for the middle as potential ans
+			int mid = start + (end - start) / 2;
+
+			// calculate how many pieces you can divide this in with this max sum
+			int sum = 0;
+			int pieces = 1;
+
+			for (int num : nums) {
+				if (sum + num > mid) {
+					// you cannot add this in this subarray, make new one
+					// say you add this num in new subarray, then sum = num
+					sum = num;
+					pieces++;
+				} else {
+					sum += num;
+				}
+			}
+			if (pieces <= m) {
+				ans = mid;
+				end = mid - 1;
+			}
+
+			else {
+				start = mid + 1;
+			}
+
+		}
+		return ans; // here start == end
+	}
+
+	public int splitArray_best(int[] nums, int m) {
+		int start = 0;
+		int end = 0;
+		int res = -1;
+
+		for (int i = 0; i < nums.length; i++) {
+			start = Math.max(start, nums[i]); // maximum element in the array
+			end += nums[i]; // sum of all elements
+		}
+
+		// binary search
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			if (isValid(nums, nums.length, m, mid) == true) {
+				res = mid;
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return res;
+	}
+
+	private static boolean isValid(int[] nums, int l, int m, int mx) {
+		int sum = 0;
+		int pieces = 1;
+
+		for (int i = 0; i < l; i++) {
+			sum += nums[i];
+
+			if (sum > mx) {
+				sum = nums[i];
+				pieces++;
+			}
+			if (pieces > m) {
+				return false;
+			}
+		}
+		return true; // <= both are possible answers--> if we have more pieces
+		// we can break that down logically into more number of pieces, but our code
+		// never actually breaks that to required number of pieces all the time.
 	}
 
 	public static void main(String[] args) {
